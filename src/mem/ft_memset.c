4bed61e372f1f62963e76f5bf5a38c799a55b026
glag <glaguyon@3E5.42angouleme.fr>
2023-12-16 17:31:43 +0100

    cool memset

--- src/mem/ft_memset.c
+++ src/mem/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 12:57:23 by glaguyon          #+#    #+#             */
-/*   Updated: 2023/12/16 16:50:48 by glaguyon         ###   ########.fr       */
+/*   Updated: 2023/12/16 17:30:50 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ void	*ft_memset(void *s, int c, size_t n)
 
 	i = 0;
 	c = (unsigned char) c;
-	cons = c + (c << 8) + (c << 16) + (c << 24)
-		+ ((size_t)(c + (c << 8) + (c << 16) + (c << 24)) << 32);
+	cons = c + (size_t)(c << 8) + (size_t)(c << 16) + ((size_t)c << 24)
+		+ ((size_t)c << 32) + ((size_t)c << 40) + ((size_t)c << 48)
+		+ ((size_t)c << 56);
 	ptr = (unsigned char *) s;
 	ptr_long = (size_t *) s;
 	while (i < n / sizeof(size_t))
